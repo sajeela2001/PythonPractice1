3cf8a235791725ad67102820ef66b5d8e805a7f6
sajeela2001 <sajeelatahir2@gmail.com>
2026-03-10 20:36:35 +0500

    Create 2D & 3D array.py

---
+++ 2D & 3D array.py
@@ -0,0 +1,14 @@
+#question 1:-
+from numpy import*
+arr1 = array([range(1,25)])
+print(arr1)
+print(' 3D arraay')
+arr2 = arr1.reshape(2,3,4)
+print(arr2)
+
+print(' 2D array')
+arr3 = arr2.reshape(4,6)
+print(arr3)
+
+
+
